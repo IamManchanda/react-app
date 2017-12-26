66b759d745ac73294ef7202e471bfab9a11b0913
Harry Manchanda <harmanmanchanda182@gmail.com>
2017-12-26 16:17:11 +0530

    Convert stateless components to stateful components!

--- src/components/person/AppPerson.js
+++ src/components/person/AppPerson.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import scoped from './_scoped.scss';
 
-const AppPerson = (props) => (
-  <div className={'app-Person auto cell callout ' + scoped.main}>
-    <h3 className={'app-Person_Title ' + scoped.title} >
-      { props.name }, { props.age }
-    </h3>
-    <p className={'app-Person_Content ' + scoped.content}>
-      He is the { props.children } of { props.country }.
-    </p>
-    <input 
-      type="text" 
-      onChange={ props.change } 
-      value={ props.name } />
-    <button 
-      className="close-button" 
-      aria-label="Dismiss alert" 
-      type="button" 
-      onClick={ props.click }>
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
-);
+class AppPerson extends Component {
+  render() {
+    return (
+      <div className={'app-Person auto cell callout ' + scoped.main}>
+        <h3 className={'app-Person_Title ' + scoped.title} >
+          { this.props.name }, { this.props.age }
+        </h3>
+        <p className={'app-Person_Content ' + scoped.content}>
+          He is the { this.props.children } of { this.props.country }.
+        </p>
+        <input 
+          type="text" 
+          onChange={ this.props.change } 
+          value={ this.props.name } />
+        <button 
+          className="close-button" 
+          aria-label="Dismiss alert" 
+          type="button" 
+          onClick={ this.props.click }>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    );
+  }
+} 
 
 export default AppPerson;
--- src/components/persons/AppPersons.js
+++ src/components/persons/AppPersons.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppPerson from './../person/AppPerson';
 
-const AppPersons = (props) => props.persons.map((person, index) => {
-  return (
-    <AppPerson
-      name={ person.name }
-      age={ person.age }
-      country={ person.country }
-      click={ () => props.clicked(index) }
-      change={ (event) => props.changed(event, person.id) }
-      key={ person.id } >
-      { person.designation }
-    </AppPerson>
-  );
-});
+class AppPersons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <AppPerson
+          name={ person.name }
+          age={ person.age }
+          country={ person.country }
+          click={ () => this.props.clicked(index) }
+          change={ (event) => this.props.changed(event, person.id) }
+          key={ person.id } >
+          { person.designation }
+        </AppPerson>
+      );
+    });
+  }
+}
+
 
 export default AppPersons;
